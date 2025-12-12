137eecb4332ddcce742f5f3e5950395c0e6fcc47
memoneno188-hue <memoneno188@gmail.com>
2025-12-12 23:29:28 +0300

    Update migration.sql

--- prisma/migrations/20251207064819_print/migration.sql
+++ prisma/migrations/20251207064819_print/migration.sql
@@ -7,11 +7,14 @@
   - Added the required column `updated_at` to the `invoice_item_templates` table without a default value. This is not possible if the table is not empty.
 
 */
--- AlterTable
-ALTER TABLE "invoice_item_templates" ADD COLUMN     "updated_at" TIMESTAMP(3) NOT NULL,
-ADD COLUMN     "vat_rate" DECIMAL(5,2) DEFAULT 0;
 
 -- AlterTable
-ALTER TABLE "invoice_items" DROP COLUMN "quantity",
-DROP COLUMN "unit_price",
-DROP COLUMN "vat_rate";
+ALTER TABLE "invoice_item_templates"
+    ADD COLUMN "updated_at" TIMESTAMP(3) NOT NULL,
+    ADD COLUMN "vat_rate" DECIMAL(5,2) DEFAULT 0;
+
+-- AlterTable
+ALTER TABLE "invoice_items"
+    DROP COLUMN IF EXISTS "quantity",
+    DROP COLUMN IF EXISTS "unit_price",
+    DROP COLUMN IF EXISTS "vat_rate";
